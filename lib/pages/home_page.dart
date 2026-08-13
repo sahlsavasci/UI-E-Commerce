1b5082e35f866022145b2b5a99e6fc3f522b232c
Sahl <sahl.savasci@gmail.com>
2026-08-13 08:26:16 +0700

    Items Widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,142 @@
-import 'package:ui_ecommerce/pages/account_page.dart';
-import 'package:ui_ecommerce/pages/cart_page.dart';
-import 'package:ui_ecommerce/widgets/CategoriesWidget.dart';
-import 'package:ui_ecommerce/widgets/HomeAppBar.dart';
-import 'package:ui_ecommerce/widgets/ItemsWidget.dart';
-import 'package:package:flutter/material.dart';
+import 'package:e_commerce/widgets/HomeAppBar.dart';
+import 'package:flutter/material.dart';
+import 'package:e_commerce/pages/account_page.dart';
+import 'package:e_commerce/pages/cart_page.dart';
+import 'package:e_commerce/widgets/CategoriesWidget.dart';
+import 'package:e_commerce/widgets/HomeAppBar.dart';
+import 'package:e_commerce/widgets/ItemsWidget.dart';
+import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+  final PageController _pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        children: const [
+          HomePageContent(),
+          CartPage(),
+          AccountPage(),
+        ],
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.transparent,
+        height: 70,
+        color: const Color(0xFF4C53A5),
+        items: const [
+          Icon(Icons.home, size: 30, color: Colors.white),
+          Icon(Icons.shopping_cart, size: 30, color: Colors.white),
+          Icon(Icons.account_circle_sharp, size: 30, color: Colors.white),
+        ],
+        index: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+            _pageController.jumpToPage(index);
+          });
+        },
+      ),
+    );
+  }
+}
+
+class HomePageContent extends StatelessWidget {
+  const HomePageContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        const HomeAppBar(),
+        Container(
+          padding: const EdgeInsets.only(top: 15),
+          decoration: const BoxDecoration(
+            color: Color(0xFFEDFCF2),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(35),
+              topRight: Radius.circular(35),
+            ),
+          ),
+          child: Column(
+            children: [
+              /* =============== Kotak Pencarian =============== */
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 15),
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Search here...",
+                        ),
+                      ),
+                    ),
+                    const Icon(
+                      Icons.camera_alt,
+                      size: 27,
+                      color: Color(0xFF4C53A5),
+                    ),
+                  ],
+                ),
+              ),
+
+              /* =============== Kategori Produk =============== */
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                child: const Text(
+                  'Categories',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF4C53A5),
+                  ),
+                ),
+              ),
+              const CategoriesWidget(),
+
+              /* =============== Produk Terlaris =============== */
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                child: const Text(
+                  'Best Selling',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF4C53A5),
+                  ),
+                ),
+              ),
+              ItemsWidget(),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
 
